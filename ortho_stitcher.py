eb80cd2471a103daa5953a3bacc83b27db716f7f
Kolad2 <koladik@gmail.com>
2024-10-07 22:05:07 +0300

    orthostitcher update

--- ortho_stitcher.py
+++ ortho_stitcher.py
@@ -17,14 +17,13 @@ class VideoFrameGetter:
 
 
 class CV2VideoStitcher:
-	def __init__(self, path_to_video, start_frame, end_frame):
+	def __init__(self, path_to_video, start_frame=None, end_frame=None):
 		video = VideoFrameGetter(path_to_video)
 		self.frame_count = video.frame_count
 		self.stitcher = cv2.Stitcher.create(cv2.Stitcher_SCANS)
 		start_frame = start_frame if start_frame is not None else 0
 		end_frame = end_frame if end_frame is not None else self.frame_count
 		self.frames = [video.get(num) for num in range(start_frame, end_frame)]
-
 
 	def stitch(self):
 		(_result, frame_result) = self.stitcher.stitch(self.frames)
